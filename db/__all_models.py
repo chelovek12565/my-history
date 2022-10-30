21fd51d7d3a38a97c23f2374f1e8accec7e36032
chelovek12565 <bas180407@mail.tu>
2022-10-30 22:19:32 +0500

    sqlalchemy

---
+++ db/__all_models.py
@@ -0,0 +1,6 @@
+from .users import User
+from .debts import Debt
+from .debtors import Debtor
+from .groups import Group, GroupMember
+from .friends import Friend
+from .notification import Group_Notification, Friend_Notification, Debt_Notification
